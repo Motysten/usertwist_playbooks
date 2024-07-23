5e7397d7bebf7378d72b7a2fd202521d663c9505
Mateo <mdm@athelas.fr>
2024-07-23 10:46:14 +0200

    Removed absolute paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-read -p "Enter root password" password
+read -p "Enter root password : " password
 
 # Update APT repos
 sudo apt update
@@ -16,8 +16,7 @@ pipx ensurepath
 pipx install --include-deps ansible
 
 # Install passlib (for ssh password connection)
-sudo apt install python3-passlib
-pipx inject ansible passlib
+sudo apt install sshpass
 
 # Make sure git is installed
 sudo apt install git
@@ -25,4 +24,4 @@ sudo apt install git
 # Clone ansible_playbooks repo
 git clone https://git.athelas-conseils.fr/Stage/ansible_playbooks.git
 
-ansible-playbook ansible_playbooks/tasks/full_setup.yml -i ansible_playbooks/inventory.ini --extra-vars "ansible_ssh_pass=$password"
+~/.local/bin/ansible-playbook ansible_playbooks/tasks/full_setup.yml -i ansible_playbooks/inventory.ini --extra-vars "ansible_ssh_pass=$password ansible_ssh_common_args='-o StrictHostKeyChecking=no'"
